c64a639b45aef15e14fde7b04688bc1ff293d116
Marcos Vinícius <catce.2022111TADS0290@aluno.ifpi.edu.br>
2022-09-19 20:28:53 -0300

    add: pasta do exercicio 7

---
+++ Exercicio-07/Parte-1/q01.ts
@@ -0,0 +1,8 @@
+// 1. Enumere os 3 tipos mais comuns de tratamento de erros e exemplifique com códigos seus ou pesquisados na internet.
+
+// 1 - Desconsiderar operação
+
+class Conta {
+	private _numero: String;
+	private _saldo: number;
+}
