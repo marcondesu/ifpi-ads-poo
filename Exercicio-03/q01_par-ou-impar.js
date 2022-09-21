ac39b4cfd7612f9175e3f2b2a56005cbee61a99c
Marcos Vinícius <catce.2022111TADS0290@aluno.ifpi.edu.br>
2022-09-21 09:24:54 -0300

    add: pasta do exercicio 03

---
+++ Exercicio-03/q01_par-ou-impar.js
@@ -0,0 +1,9 @@
+function parOuImpar(number) {
+    if (number % 2) {
+        return false;
+    }
+    return true;
+}
+console.log(parOuImpar(2));
+console.log(parOuImpar(3));
+console.log(parOuImpar(0));
---
+++ Exercicio-03/q01_par-ou-impar.ts
@@ -0,0 +1,11 @@
+function parOuImpar(number: number): boolean {
+    if (number % 2) {
+        return false
+    }
+
+    return true
+}
+
+console.log(parOuImpar(2))
+console.log(parOuImpar(3))
+console.log(parOuImpar(0))
